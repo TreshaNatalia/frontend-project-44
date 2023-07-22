b6f92ca51ee3964bf6f1d450949e494d76dc4b67
Natalia Trekhsvoiakova <treshka.natashka77@gmail.com>
2023-07-22 20:41:42 +0300

    step four eslint

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlinesync from 'readline-sync';
 
 const userName = () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlinesync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+  console.log('Welcome to the Brain Games!');
+  const name = readlinesync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 export default userName;

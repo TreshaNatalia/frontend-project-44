cc7bfae30b316075933911e03c58835594ba9ecf
Natalia Trekhsvoiakova <treshka.natashka77@gmail.com>
2023-07-26 22:36:33 +0300

    add step5

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/TreshaNatalia/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/TreshaNatalia/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/699655474f7d400f8dc9/maintainability)](https://codeclimate.com/github/TreshaNatalia/frontend-project-44/maintainability)
-[![asciicast](https://asciinema.org/a/tM5Yd01mA3op4VeJtkMNcwlfe.svg)][def]
-
-
-[def]: https://asciinema.org/a/tM5Yd01mA3op4VeJtkMNcwlfe
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,14 +6,14 @@ const getRandomNumber = () => Math.round(Math.random() * 100);
 function startRound() {
   const randomNumber = getRandomNumber();
   const isEven = randomNumber % 2 === 0;
-  const answer = readlinesync.question(`Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${randomNumber} `);
+  console.log(`Question: ${randomNumber}`);
   const correctAnswer = (isEven) ? 'yes' : 'no';
-  console.log(`Your answer: ${correctAnswer}`);
+  const answer = readlinesync.question('Your answer: ');
   if (answer.toLowerCase() === correctAnswer) {
     console.log('Correct!');
     return true;
   }
-  console.log(`Wrong: ${correctAnswer}.`);
+  console.log(`'${answer}' is wrong answer ;(.  Correct answer was '${correctAnswer}'.`);
   return false;
 }
 // eslint-disable-next-line consistent-return
@@ -21,10 +21,11 @@ function startGame() {
   console.log('Welcome to the Brain Games!');
   const name = readlinesync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const isCorrect = startRound();
     if (!isCorrect) {
-      return console.log('Game over!');
+      return console.log(`Let's try again, ${name}!`);
     }
   }
   console.log(`Congratulations, ${name}!`);
